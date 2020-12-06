efff1be25861f2aa11353a4367ecd03851814146
Brad Grzesiak <brad@bendyworks.com>
2020-12-05 21:40:49 -0600

    Day 5 golfing

--- 5/part2.rb
+++ 5/part2.rb
@@ -1,5 +1,5 @@
-puts(File.read('./input.txt').split("\n").map do |data|
-  data.tr('FL', '0').tr('BR', '1').to_i(2)
+puts(File.foreach('./input.txt').map do |d|
+  d.tr('FLBR', '0011').to_i(2)
 end.sort.chunk_while do |bef, aft|
   bef + 1 == aft
 end.first.last + 1)
