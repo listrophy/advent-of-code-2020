a1cc188b77a317b395621fd6633e811801c0c8e8
Brad Grzesiak <brad@bendyworks.com>
2020-12-12 07:30:38 -0600

    Use complex for Day 12 part 1

--- 12/part1.rb
+++ 12/part1.rb
@@ -1,21 +1,18 @@
-dir = 0
-puts(File.foreach('./input.txt').reduce([0, 0]) do |(x, y), l|
+require 'complex'
+
+I = Complex::I
+
+puts(File.foreach('./input.txt').reduce([Complex(0, 0), 1]) do |(n, dir), l|
   _, i, amt_s = l.match(/^(\w)(\d+)$/).to_a
   amt = amt_s.to_i
   case i
-  when ?N then y += amt
-  when ?S then y -= amt
-  when ?E then x += amt
-  when ?W then x -= amt
-  when ?L then dir += amt / 90
-  when ?R then dir -= amt / 90
-  when ?F
-    case dir % 4
-    when 0 then x += amt
-    when 1 then y += amt
-    when 2 then x -= amt
-    when 3 then y -= amt
-    end
+  when ?N then n += amt * I
+  when ?S then n -= amt * I
+  when ?E then n += amt
+  when ?W then n -= amt
+  when ?L then (amt / 90).times { dir *= I }
+  when ?R then (amt / 90).times { dir *= -I }
+  when ?F then n += amt * dir
   end
-  [x, y]
-end.map(&:abs).sum)
+  [n, dir]
+end[0].rect.map(&:abs).sum)
